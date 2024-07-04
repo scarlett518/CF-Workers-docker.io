50aa6e5f06bfba242bf89d39420e101ca7fa2c23
scarlett518 <86555463+scarlett518@users.noreply.github.com>
2024-07-04 19:37:02 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -114,6 +114,7 @@ async function generateHelpHTML(env) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>镜像加速说明</title>
+          <h2>使用方法①</h2>
     <style>
         body {
             font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
