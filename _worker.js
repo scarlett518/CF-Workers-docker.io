7e4040f24830b0b8dde3ce45db4fc7d009904960
scarlett518 <86555463+scarlett518@users.noreply.github.com>
2024-07-04 19:22:49 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -202,7 +202,6 @@ async function generateHelpHTML(env) {
         <center><h4>个人自建+整理，请勿随意分享，导致资源滥用无法使用</h4></center>
         <h3>为了加速镜像拉取，你可以使用以下命令设置 registry mirror:</h3>
         <pre><code>
-sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
   "registry-mirrors": [
